feec5eaad7896ad1b05ef80af6c66d77c5166463
wueiz <wueiz@wueizs-MacBook-Pro.local>
2017-07-17 11:06:35 +0800

    use angular/material~

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/global/header/header.component';
+import { FooterComponent } from './pages/global/footer/footer.component';
+import { IndexComponent } from './pages/index/index.component';
+import { AboutComponent } from './pages/about/about.component';
+
+// place to import material
+import {MdButtonModule} from '@angular/material';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
