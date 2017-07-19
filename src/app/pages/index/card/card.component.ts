599b1511f673967c463401b39743764e8ab89076
wueiz <wueiz@wueizs-MacBook-Pro.local>
2017-07-19 16:22:11 +0800

    update the card component to suitable the input label

--- src/app/pages/index/card/card.component.ts
+++ src/app/pages/index/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article_preview } from '../../../models/article_preview';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
+  @Input()
+  article: Article_preview;
   constructor() { }
 
   ngOnInit() {
