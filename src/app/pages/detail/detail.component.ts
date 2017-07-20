a00828116c9e1fe6ecc3e9c41e805845b0991e29
wueiz <wueiz@wueizs-MacBook-Pro.local>
2017-07-21 01:41:04 +0800

    design the ui and upload to use

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { ArticleGetServiceService } from '../../services/article-get-service.service';
+
+import { Article } from '../../models/article';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
-
-  constructor() { }
+  article: Article;
+  constructor(
+    private articleService: ArticleGetServiceService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe( res => {
+      this.article = this.articleService.getArticle(+res['id']);
+      console.log(this.article);
+    })
   }
 
 }
